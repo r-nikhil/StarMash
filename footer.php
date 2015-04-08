feb1d3475ffb37b385aff40a6d7644d2546cc3f0
Nikhil R <rnikhil2751996@gmail.com>
2015-04-08 12:38:10 +0530

    Add footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <p class="panel-footer panel-collapse ">Based on ELO rating system. For Questions Ping !NF@M0Us</p>
+
+
+
+            </div>
+
+
+        </div>
+
+
+
+
+    </div>
